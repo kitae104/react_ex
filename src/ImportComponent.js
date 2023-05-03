f35604463adef461bd011e4d24e68efca7db3c1f
Kitae's Github <aqua0405@gmail.com>
2023-05-04 01:50:25 +0900

    Create ImportComponent.js

---
+++ src/ImportComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ImportComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>[ THIS IS IMPORTED COMPONENT ]</h2>
+      </div>
+    );
+  }
+}
+
+export default ImportComponent;
