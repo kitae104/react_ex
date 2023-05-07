338c091f9ca6243e8101d2e70ccd72dd2179cd83
Kitae's Github <aqua0405@gmail.com>
2023-05-07 18:59:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SpreadOperator from './basic/SpreadOperator';
+import ArrowFuncion from './basic/ArrowFuncion';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <h1>인하공전 컴시과 23'</h1>
       <p>CSS 적용하기</p>
       <p>This is my first React compoment</p>
-      <SpreadOperator />
+      <ArrowFuncion />
     </>
   );
 }
