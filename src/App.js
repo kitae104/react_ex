c28fb0f4ad5e3e7ca061322133d26857970f2b20
Kitae's Github <aqua0405@gmail.com>
2023-05-14 02:26:49 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-import Jquery from './basic/Jquery';
+import PropsDatatypes from './basic/PropsDatatypes';
 
 function App() {
   return (
     <>
       <h1>인하공전 컴시과 23'</h1>
       <p>CSS 적용하기</p>
-      <p>This is my first React compoment</p>
-      <Jquery />
+      <PropsDatatypes 
+        String="Data"
+        Number={123}
+        Boolean={1==1}
+        Array={[0, 1, 8]}
+        ObjectJson={{name: "홍길동", age: 21}}
+        Function={console.log("FunctionProps: function!")}
+      />
     </>
   );
 }
