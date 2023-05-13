3fe16dcd1c7df2ff60cb3b61fa037579a66fdeb2
Kitae's Github <aqua0405@gmail.com>
2023-05-14 02:26:56 +0900

    Create PropsDatatypes.js

---
+++ src/basic/PropsDatatypes.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import datatype from 'prop-types';
+
+class PropsDatatypes extends Component {
+  render() {
+    let {
+      String, Number, Boolean, Array, ObjectJson, Function
+    } = this.props;
+
+    return (
+      <div style={{padding: "0px"}}>
+        <p>StringProps : {String}</p>
+        <p>NumberProps : {Number}</p>
+        <span>BooleanProps : {Boolean.toString()}</span>
+        <p>ArrayProps : {Array.toString()}</p>
+        <p>ObjectJsonProps : {JSON.stringify(ObjectJson)}</p>
+        <p>FunctionProps : {Function}</p>
+      </div>
+    );
+  }
+}
+
+PropsDatatypes.propTypes = {
+  String: datatype.string,
+  Number: datatype.number,
+  Boolean: datatype.bool,
+  Array: datatype.array,
+  ObjectJson: datatype.object,
+  Function: datatype.func,
+}
+
+export default PropsDatatypes;
